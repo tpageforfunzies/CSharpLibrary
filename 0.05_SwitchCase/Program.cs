992a422b94475d52b8b53f3979d7043434e2b88f
tpageforfunzies <tjpage@indiana.edu>
2017-10-25 14:26:18 -0400

    switch case demo

---
+++ 0.05_SwitchCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Taylor";
+
+            switch (friend)
+            {
+                case "Taylor":
+                    Console.WriteLine("WHAT UP THO.");
+                    break;
+                case "Paul":
+                    Console.WriteLine("Paul is pretty cool too.");
+                    break;
+                case "Kenn":
+                    Console.WriteLine("Silent master Kenn . . .");
+                    break;
+                default:
+                    Console.WriteLine("I don't know you");
+                    break;
+            }
+        }
+    }
+}
