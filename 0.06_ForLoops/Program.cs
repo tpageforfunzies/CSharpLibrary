512a423278f0944067e9277de6e31a41beb5219d
tpageforfunzies <tjpage@indiana.edu>
2017-10-25 14:52:20 -0400

    add for loops file

---
+++ 0.06_ForLoops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._06_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
