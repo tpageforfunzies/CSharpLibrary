d4e15c8c98ef8d1d1ae4cf6759a63b1a6d7dd822
tpageforfunzies <tjpage@indiana.edu>
2017-10-26 13:33:51 -0400

    finished euler5

---
+++ 0.09_CollectionsDictionaries/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._09_CollectionsDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 1.05_Euler5/Euler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._05_Euler5
+{
+    class Euler
+    {
+        public static int smallestMultiple()
+        {
+            //starting the number we're checking at 1
+            int number = 1;
+            //will loop through all of the divisors up to 20
+            for (int i = 1; i <= 20; i++)
+            {
+                //if the number is not evenly divisible, increment number and start over
+                if (number % i != 0)
+                {
+                    //increment the number up by one
+                    number++;
+                    //reset the index, restarting the for loop
+                    i = 1;
+                }
+            }
+            return number;
+        }
+    }
+}
