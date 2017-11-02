3064b42697103d5a255d3d488f97735843f31acd
tpageforfunzies <tjpage@indiana.edu>
2017-11-02 09:37:30 -0400

    quiz one, part one

---
+++ 0.18_QuizOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._18_QuizOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangleOne = new Triangle(1, 2);
+            Triangle triangleTwo = new Triangle(9, 1);
+            Triangle triangleThree = new Triangle(4, 7);
+            Triangle triangleFour = new Triangle(2, 2);
+            Triangle triangleFive = new Triangle(7, 5);
+
+            Console.WriteLine(triangleTwo.Area());
+        }
+    }
+}
---
+++ 0.18_QuizOne/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._18_QuizOne
+{
+    class Triangle : Shape
+    {
+        public int Base { get; set; }
+        public int Height { get; set; }
+        public double TArea { get; set; }
+        
+
+        public Triangle(int TBase, int THeight)
+        {
+            this.Base = TBase;
+            this.Height = THeight;
+        }
+
+        public override double Area()
+        {
+            
+            TArea = (Base * Height * 0.5);
+            return TArea;
+        }
+    }
+}
